f035970a4600be4d72e98dce1edab338020b5c23
HouseMech <liam.harbec@gmail.com>
2024-03-07 14:14:15 -0800

    Added functions to acquire the base folder and the asset folder

--- lib/token_functions.rb
+++ lib/token_functions.rb
@@ -11,35 +11,49 @@ end
 
 private
 def create_composites(image_path, asset_folder_name, dir_name)
-  dir_name = File.basename(dir_name, ".png")
   base_image = check_image_exists(image_path)
-  create_token_directory(dir_name)
-  
-  get_asset_names().each_with_index do |asset, index|
-    asset_image = Magick::Image.read(asset).first
-    
-    # Composite the asset image onto the base image
-    composited_image = base_image.composite(asset_image, Magick::CenterGravity, Magick::OverCompositeOp)
-    
-    # Save the composited image
-     if asset.include? "number" 
-      output_filename = File.join(dir_name, "#{File.basename(image_path, ".png")}_#{index}.png")
-     else 
-      output_filename = File.join(dir_name, "#{File.basename(image_path, ".png")}_#{File.basename(asset, ".png")}.png")
-     end
-    composited_image.write(output_filename)
-    puts "Saved: #{output_filename}"
-  end
+  if base_image then
+    create_token_directory(root_path+dir_name)
 
-  puts "Finished creating token images."
+    get_asset_names().each_with_index do |asset, index|
+      asset_image = Magick::Image.read(asset).first
+
+      # Composite the asset image onto the base image
+      composited_image = base_image.composite(asset_image, Magick::CenterGravity, Magick::OverCompositeOp)
+
+      # Save the composited image
+       if asset.include? "number"
+        output_filename = File.join(root_path+dir_name, "#{File.basename(image_path, ".png")}_#{index}.png")
+       else
+        output_filename = File.join(root_path+dir_name, "#{File.basename(image_path, ".png")}_#{File.basename(asset, ".png")}.png")
+       end
+      composited_image.write(output_filename)
+      puts "Saved: #{output_filename}"
+    end
+
+    puts "Finished creating token images."
+  else
+    puts "Failed to create images"
+  end
 end
 
 def create_token_directory(dir_name)
   Dir.mkdir(dir_name) unless Dir.exist?(dir_name)
 end
 
+def assets_path
+  # __dir__ returns the directory of the current file.
+  # Using File.join ensures compatibility across OSes by using the correct directory separators.
+  # File.expand_path is used to navigate up to the project's root directory then down to 'lib/assets'.
+  File.expand_path(File.join(root_path, 'lib', 'assets'))
+end
+
+def root_path
+  File.expand_path(File.join(__dir__, '..'))
+end
+
 def get_asset_names()
-  return Dir["../lib/assets/*.png"]
+  return Dir[assets_path+"/*.png"]
 end
 
 def check_image_exists(image_path)
@@ -47,7 +61,7 @@ def check_image_exists(image_path)
     base_image = Magick::Image.read(image_path).first
   rescue
     puts "Could not open the base image: #{image_path}"
-    exit
+    return nil
   end
   return base_image
 end
