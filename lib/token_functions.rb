a8bf882a296b253cde2cdbff79a737d7febb1fe5
HouseMech <liam.harbec@gmail.com>
2024-04-16 15:41:16 -0700

    Renamed some functions to better fit their titles. Added a helper function to resize asset files for different resolutions

--- lib/token_functions.rb
+++ lib/token_functions.rb
@@ -12,13 +12,13 @@ end
 private
 
 def create_composites(image_path, asset_folder, dir_name)
-  base_image = check_image_exists(image_path)
+  base_image = get_image_if_exists(image_path)
   return unless base_image
 
   create_token_directory(root_path + dir_name)
 
   asset_names(asset_folder).each do |asset|
-    asset_image = Magick::Image.read(asset).first
+    asset_image = get_asset_image_and_resize(asset, base_image.columns, base_image.rows)
     # Composite the asset image onto the base image
     composited_image = create_composite_image(base_image, asset_image)
     # Save the composited image
@@ -27,13 +27,26 @@ def create_composites(image_path, asset_folder, dir_name)
   end
 end
 
+def get_asset_image_and_resize(asset, base_image_width, base_image_height)
+  asset_image = get_image_if_exists(asset)
+  if (base_image_width && base_image_height) && (base_image_width != 256 && base_image_height != 256)
+    asset_image = resize(asset_image, base_image_width,
+                         base_image_height)
+  end
+  asset_image
+end
+
 def save_composite_image(composited_image, output_filename)
   composited_image.write(output_filename)
   puts "Saved: #{output_filename}"
 end
 
-def create_composite_image(base_image, asset_image)
-  base_image.composite(asset_image, Magick::CenterGravity, Magick::OverCompositeOp)
+def create_composite_image(image, asset_image)
+  image.composite(asset_image, Magick::CenterGravity, Magick::OverCompositeOp)
+end
+
+def resize(image, width, height)
+  image.resize_to_fit(width, height)
 end
 
 def create_output_filename(asset, image_path, dir_name)
@@ -56,7 +69,7 @@ def asset_names(path_to_asset_folder)
   Dir["#{path_to_asset_folder == 'assets' ? assets_path : path_to_asset_folder}/*.png"]
 end
 
-def check_image_exists(image_path)
+def get_image_if_exists(image_path)
   begin
     base_image = Magick::Image.read(image_path).first
   rescue StandardError
